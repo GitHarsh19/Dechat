38483db5dbd4be06ce1bc0bb4aa03801b6b36f96
harshit <harshitagarwalsmt@gmail.com>
2023-08-23 01:17:43 +0530

    sending message from ui to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to:'sessions#destroy'
+  post 'message', to: 'messages#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
