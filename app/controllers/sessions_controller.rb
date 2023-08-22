dfdcef0dae137c3fbcddcaa3f507dbc075706867
harshit <harshitagarwalsmt@gmail.com>
2023-08-23 00:53:46 +0530

    adding restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  
+  before_action :logged_in_redirect, only: [:new, :create]
   def new
   end
   def create
@@ -17,4 +19,12 @@ class SessionsController < ApplicationController
     flash[:success]="You have Successfully Logged Out"
     redirect_to login_path
   end
+  private
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You Are Already Logged In"
+      redirect_to root_path
+    end
+  end
+
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,12 @@ import "semantic-ui"
 $('.ui.dropdown')
   .dropdown()
 ;
+
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
