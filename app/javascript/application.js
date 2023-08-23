727190f5de2f465ff378ecdc94f1dbd39357ba45
harshit <harshitagarwalsmt@gmail.com>
2023-08-23 19:07:43 +0530

    project done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,12 +8,19 @@ $('.ui.dropdown')
   .dropdown()
 ;
 
+scroll_bottom = function(){
+  if ($('#messages').length > 0){
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $('.message .close')
   .on('click', function() {
     $(this)
       .closest('.message')
-      .transition('fade')
-    ;
-  })
-;
+      .transition('fade');
+
+  });
+scroll_bottom();
+
 import "./channels"
